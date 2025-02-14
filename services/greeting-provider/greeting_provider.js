d0930fbe78531078d3f66584f7af6f273d30b2bd
johannes karoff <johannes.karoff@hivemindtechnologies.com>
2025-02-14 14:41:31 +0400

    log greeting in greeting-provider

--- services/greeting-provider/greeting_provider.js
+++ services/greeting-provider/greeting_provider.js
@@ -19,7 +19,9 @@ function getRandomGreeting() {
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(getRandomGreeting());
+    const greeting = getRandomGreeting();
+    console.log(greeting);
+    res.end(greeting);
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('404 Not Found');
