b6c366c29ea1436090cc848e627139be0ccf29a1
johannes karoff <johannes.karoff@hivemindtechnologies.com>
2025-02-18 13:38:39 +0400

    fix greeting_provider encoding

--- services/greeting-provider/greeting_provider.js
+++ services/greeting-provider/greeting_provider.js
@@ -18,7 +18,7 @@ function getRandomGreeting() {
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
     const greeting = getRandomGreeting();
     console.log(greeting);
     res.end(greeting);
